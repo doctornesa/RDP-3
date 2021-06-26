b81a5a7e71738b38050142048cab179e45d422f2
ans2004 <79177788+ans2004@users.noreply.github.com>
2021-06-26 17:00:10 +0200

    Update NGROK-CHECK.bat

--- FILES/NGROK-CHECK.bat
+++ FILES/NGROK-CHECK.bat
@@ -5,8 +5,8 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" 
 curl -O https://raw.githubusercontent.com/ans2004/RDP/main/FILES/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Telegram.exe" https://raw.githubusercontent.com/ans2004/RDP/main/FILES/Telegram.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\winrar_SETUP.exe" https://raw.githubusercontent.com/ans2004/RDP/main/FILES/winrar-x64-602ar.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\SOCKS4.txt" https://cutt.us/ans2004 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\SOCKS5.txt" https://cutt.us/ans20041 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\SOCKS4 BY ANAS.txt" https://2u.pw/7OBRt > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\SOCKS5 BY ANAS.txt" https://2u.pw/FR2Vr > out.txt 2>&1
 net user administrator @ans2004123 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
