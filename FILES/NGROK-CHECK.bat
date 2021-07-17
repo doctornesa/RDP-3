4fae824e29d1c583d1178e2f2c4e141e23d475dc
T36k0 <38859474+T36k0@users.noreply.github.com>
2021-07-17 12:55:46 +0300

    Update NGROK-CHECK.bat

--- FILES/NGROK-CHECK.bat
+++ FILES/NGROK-CHECK.bat
@@ -4,7 +4,7 @@ net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -O https://raw.githubusercontent.com/T36k0/RDP/main/FILES/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" "https://raw.githubusercontent.com/T36k0/RDP/main/FILES/FastConfigVPS_v5.1.exe" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\OpenRPA.msi" "https://github-releases.githubusercontent.com/185806136/0c57d502-4c4c-4647-be55-cebdabf52720?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210717%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210717T094451Z&X-Amz-Expires=300&X-Amz-Signature=5b4ab766311ee0e7301c17504f1bbf658883608bb89d4a64800a8b1826c96a9d&X-Amz-SignedHeaders=host&actor_id=38859474&key_id=0&repo_id=185806136&response-content-disposition=attachment%3B%20filename%3DOpenRPA.msi&response-content-type=application%2Foctet-stream" > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\OpenRPA.msi" "https://github.com/open-rpa/openrpa/releases/latest/download/OpenRPA.msi" > out.txt 2>&1
 
 net user tishko @testaa1 /add >nul
 net user rdp1 @testaa1 /add >nul
