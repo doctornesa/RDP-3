b7e0d75153dc0d85aa7dfea172ec6189bfbd2f33
T36k0 <38859474+T36k0@users.noreply.github.com>
2021-07-17 10:22:56 +0300

    Update NGROK-CHECK.bat

--- FILES/NGROK-CHECK.bat
+++ FILES/NGROK-CHECK.bat
@@ -2,26 +2,31 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-curl -O https://raw.githubusercontent.com/ans2004/RDP/main/FILES/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Telegram.exe" "https://raw.githubusercontent.com/ans2004/RDP/main/FILES/Telegram.exe" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" "https://raw.githubusercontent.com/ans2004/RDP/main/FILES/FastConfigVPS_v5.1.exe" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\winrar_SETUP.exe" "https://raw.githubusercontent.com/ans2004/RDP/main/FILES/winrar-x64-602ar.exe" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\HTTP BY ANAS.txt" "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\SOCKS4 BY ANAS.txt" "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all&simplified=true" > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\SOCKS5 BY ANAS.txt" "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all" > out.txt 2>&1
-net user administrator @ans2004123 /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+curl -O https://raw.githubusercontent.com/T36k0/RDP/main/FILES/DisablePasswordComplexity.ps1?token=AJIPFUVFPML3TEJ7XRAVO7TA6KB2M > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" "https://raw.githubusercontent.com/T36k0/RDP/main/FILES/FastConfigVPS_v5.1.exe?token=AJIPFUU2YN6LCYWALAG65F3A6KCA6" > out.txt 2>&1
 
-echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
+net user tishko testaa1 /add >nul
+net user rdp1 testaa1 /add >nul
+net user rdp2 testaa1 /add >nul
+net user rdp3 testaa1 /add >nul
+net localgroup administrators tishko /add >nul
+net localgroup administrators rdp1 /add >nul
+net localgroup administrators rdp2 /add >nul
+net localgroup administrators rdp3 /add >nul
+net user tishko /active:yes >nul
+net user rdp1 /active:yes >nul
+net user rdp2 /active:yes >nul
+net user rdp3 /active:yes >nul
+
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
 echo User: Administrator
-echo Pass: @ans2004123
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant Users:F >nul
+ICACLS C:\Windows\installer /grant Users:F >nul
 ping -n 10 127.0.0.1 >nul
