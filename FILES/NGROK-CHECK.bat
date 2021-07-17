1a2edc323a1503412d7e6cde0120644c44841680
T36k0 <38859474+T36k0@users.noreply.github.com>
2021-07-17 12:31:42 +0300

    Update NGROK-CHECK.bat

--- FILES/NGROK-CHECK.bat
+++ FILES/NGROK-CHECK.bat
@@ -5,9 +5,18 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" 
 curl -O https://raw.githubusercontent.com/T36k0/RDP/main/FILES/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" "https://raw.githubusercontent.com/T36k0/RDP/main/FILES/FastConfigVPS_v5.1.exe" > out.txt 2>&1
 
-net user administrator testaa1 /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user tishko @testaa1 /add >nul
+net user rdp1 @testaa1 /add >nul
+net user rdp2 @testaa1 /add >nul
+net user rdp3 @testaa1 /add >nul
+net localgroup administrators tishko /add >nul
+net localgroup administrators rdp1 /add >nul
+net localgroup administrators rdp2 /add >nul
+net localgroup administrators rdp3 /add >nul
+net user tishko /active:yes >nul
+net user rdp1 /active:yes >nul
+net user rdp2 /active:yes >nul
+net user rdp3 /active:yes >nul
 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
